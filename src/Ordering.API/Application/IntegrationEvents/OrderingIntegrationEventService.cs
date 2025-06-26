e16744950bf0b753d430d9509e7bdc0f36f94eb8
Alexander Tarvid <atarvid@inbox.lv>
2025-06-26 15:44:54 +0300

    First attempt

--- src/Ordering.API/Application/IntegrationEvents/OrderingIntegrationEventService.cs
+++ src/Ordering.API/Application/IntegrationEvents/OrderingIntegrationEventService.cs
@@ -5,38 +5,33 @@ public class OrderingIntegrationEventService(IEventBus eventBus,
     IIntegrationEventLogService integrationEventLogService,
     ILogger<OrderingIntegrationEventService> logger) : IOrderingIntegrationEventService
 {
-    private readonly IEventBus _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
-    private readonly OrderingContext _orderingContext = orderingContext ?? throw new ArgumentNullException(nameof(orderingContext));
-    private readonly IIntegrationEventLogService _eventLogService = integrationEventLogService ?? throw new ArgumentNullException(nameof(integrationEventLogService));
-    private readonly ILogger<OrderingIntegrationEventService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
     public async Task PublishEventsThroughEventBusAsync(Guid transactionId)
     {
-        var pendingLogEvents = await _eventLogService.RetrieveEventLogsPendingToPublishAsync(transactionId);
+        var pendingLogEvents = await integrationEventLogService.RetrieveEventLogsPendingToPublishAsync(transactionId);
 
         foreach (var logEvt in pendingLogEvents)
         {
-            _logger.LogInformation("Publishing integration event: {IntegrationEventId} - ({@IntegrationEvent})", logEvt.EventId, logEvt.IntegrationEvent);
+            logger.LogInformation("Publishing integration event: {IntegrationEventId} - ({@IntegrationEvent})", logEvt.EventId, logEvt.IntegrationEvent);
 
             try
             {
-                await _eventLogService.MarkEventAsInProgressAsync(logEvt.EventId);
-                await _eventBus.PublishAsync(logEvt.IntegrationEvent);
-                await _eventLogService.MarkEventAsPublishedAsync(logEvt.EventId);
+                await integrationEventLogService.MarkEventAsInProgressAsync(logEvt.EventId);
+                await eventBus.PublishAsync(logEvt.IntegrationEvent);
+                await integrationEventLogService.MarkEventAsPublishedAsync(logEvt.EventId);
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error publishing integration event: {IntegrationEventId}", logEvt.EventId);
+                logger.LogError(ex, "Error publishing integration event: {IntegrationEventId}", logEvt.EventId);
 
-                await _eventLogService.MarkEventAsFailedAsync(logEvt.EventId);
+                await integrationEventLogService.MarkEventAsFailedAsync(logEvt.EventId);
             }
         }
     }
 
     public async Task AddAndSaveEventAsync(IntegrationEvent evt)
     {
-        _logger.LogInformation("Enqueuing integration event {IntegrationEventId} to repository ({@IntegrationEvent})", evt.Id, evt);
+        logger.LogInformation("Enqueuing integration event {IntegrationEventId} to repository ({@IntegrationEvent})", evt.Id, evt);
 
-        await _eventLogService.SaveEventAsync(evt, _orderingContext.GetCurrentTransaction());
+        await integrationEventLogService.SaveEventAsync(evt, orderingContext.GetCurrentTransaction());
     }
 }
